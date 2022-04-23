1b4c7270f92fa5e758558fb89efa1c4389f47e58
Nathan Ag <nathanaguila22@gmail.com>
2022-04-23 04:30:36 -0500

    Updated Functionality, needs style fixing

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -1,190 +1,209 @@
-var wordBlank = document.querySelector(".word-blanks");
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
+//Questions from https://www.interviewbit.com/javascript-mcq/
+const Questions = [
+    {
+        id: 0,
+        q: "Javascript is an _______ language?",
+        a: [
+            {text: "Object-Oriented", isCorrect: true },
+            {text: "Object-Based", isCorrect: false },
+            {text: "Procedural", isCorrect: false },
+            {text: "None of the above", isCorrect: false },
+        ]        
+    },
+    {
+        id: 1,
+        q: "Which of the following keywords is used to define a variable in Javascript?",
+        a: [
+            {text: "var", isCorrect: false },
+            {text: "let", isCorrect: false },
+            {text: "Both A and B", isCorrect: true },
+            {text: "None of the above", isCorrect: false },
+        ]  
+    },
+    {
+        id: 2,
+        q: "Which of the following methods is used to access HTML elements using Javascript?",
+        a: [
+            {text: "getElementbyId()", isCorrect: false },
+            {text: "getElementByClassName()", isCorrect: false },
+            {text: "Both A and B", isCorrect: true },
+            {text: "None of the above", isCorrect: false },
+        ]  
+    },
+    {
+        id: 3,
+        q: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        a: [
+            {text: "Throws an error", isCorrect: false },
+            {text: "Ignores the statements", isCorrect: true },
+            {text: "Gives us a warning", isCorrect: false },
+            {text: "None of the above", isCorrect: false },
+        ]  
+    },
+    {
+        id: 4,
+        q: "Which of the following methods can be used to display data in some form using Javascript?",
+        a: [
+            {text: "document.write()", isCorrect: false },
+            {text: "console.log()", isCorrect: false },
+            {text: "window.alert()", isCorrect: false },
+            {text: "All of the above", isCorrect: true },
+        ]  
+    },
+    {
+        id: 5,
+        q: "How can a datatype be declared to be a constant type?",
+        a: [
+            {text: "const", isCorrect: true },
+            {text: "var", isCorrect: false },
+            {text: "let", isCorrect: false },
+            {text: "constant", isCorrect: false },
+        ]  
+    },
+    {
+        id: 6,
+        q: "What keyword is used to check whether a given property is valid or not?",
+        a: [
+            {text: "in", isCorrect: true },
+            {text: "is in", isCorrect: false },
+            {text: "exists", isCorrect: false },
+            {text: "lies", isCorrect: false },
+        ]  
+    },
+    {
+        id: 7,
+        q: "When an operator's value is NULL, the typeof returned by the unary operator is:",
+        a: [
+            {text: "Boolean", isCorrect: false },
+            {text: "Undefined", isCorrect: false },
+            {text: "Object", isCorrect: true },
+            {text: "Integer", isCorrect: false },
+        ]  
+    },
+    {
+        id: 8,
+        q: "What does the Javascript “debugger” statement do?",
+        a: [
+            {text: "It will debug all the errors in the program at runtime", isCorrect: false },
+            {text: "It acts as a breakpoint in a program", isCorrect: true },
+            {text: "It will debug error in the current statement if any", isCorrect: false },
+            {text: "All of the above", isCorrect: false },
+        ]  
+    },
+    {
+        id: 9,
+        q: "Which function is used to serialize an object into a JSON string in Javascript?",
+        a: [
+            {text: "stringify()", isCorrect: true },
+            {text: "parse()", isCorrect: false },
+            {text: "convert()", isCorrect: false },
+            {text: "None of the above", isCorrect: false },
+        ]  
+    },
+];
 
-var chosenWord = "";
-var numBlanks = 0;
-var winCounter = 0;
-var loseCounter = 0;
-var isWin = false;
-var timer;
-var timerCount;
-
-// Arrays used to create blanks and letters on screen
-var lettersInChosenWord = [];
-var blanksLetters = [];
-
-// Array of questions the user will guess
-var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
-
-// The init function is called when the page loads 
-function init() {
-  getWins();
-  getlosses();
+// Set start
+var start = true;
+  
+// Iterate
+function iterate(id) {
+  
+    // Getting the result display section
+    var result = document.getElementsByClassName("result");
+    result[0].innerText = "";
+  
+    // Getting the question
+    const question = document.getElementById("question");
+  
+  
+    // Setting the question text
+    question.innerText = Questions[id].q;
+  
+    // Getting the options
+    const option1 = document.getElementById('option1');
+    const option2 = document.getElementById('option2');
+    const option3 = document.getElementById('option3');
+    const option4 = document.getElementById('option4');
+  
+  
+    // Providing option text 
+    option1.innerText = Questions[id].a[0].text;
+    option2.innerText = Questions[id].a[1].text;
+    option3.innerText = Questions[id].a[2].text;
+    option4.innerText = Questions[id].a[3].text;
+  
+    // Providing the true or false value to the options
+    option1.value = Questions[id].a[0].isCorrect;
+    option2.value = Questions[id].a[1].isCorrect;
+    option3.value = Questions[id].a[2].isCorrect;
+    option4.value = Questions[id].a[3].isCorrect;
+  
+    var selected = "";
+  
+    // Show selection for op1
+    option1.addEventListener("click", () => {
+        option1.style.backgroundColor = "lightgoldenrodyellow";
+        option2.style.backgroundColor = "lightskyblue";
+        option3.style.backgroundColor = "lightskyblue";
+        option4.style.backgroundColor = "lightskyblue";
+        selected = option1.value;
+    })
+  
+    // Show selection for op2
+    option2.addEventListener("click", () => {
+        option1.style.backgroundColor = "lightskyblue";
+        option2.style.backgroundColor = "lightgoldenrodyellow";
+        option3.style.backgroundColor = "lightskyblue";
+        option4.style.backgroundColor = "lightskyblue";
+        selected = option2.value;
+    })
+  
+    // Show selection for op3
+    option3.addEventListener("click", () => {
+        option1.style.backgroundColor = "lightskyblue";
+        option2.style.backgroundColor = "lightskyblue";
+        option3.style.backgroundColor = "lightgoldenrodyellow";
+        option4.style.backgroundColor = "lightskyblue";
+        selected = option3.value;
+    })
+  
+    // Show selection for op4
+    option4.addEventListener("click", () => {
+        option1.style.backgroundColor = "lightskyblue";
+        option2.style.backgroundColor = "lightskyblue";
+        option3.style.backgroundColor = "lightskyblue";
+        option4.style.backgroundColor = "lightgoldenrodyellow";
+        selected = option4.value;
+    })
+  
+    // Grabbing the evaluate button
+    const evaluate = document.getElementsByClassName("evaluate");
+  
+    // Evaluate method
+    evaluate[0].addEventListener("click", () => {
+        if (selected == "true") {
+            result[0].innerHTML = "True";
+            result[0].style.color = "green";
+        } else {
+            result[0].innerHTML = "False";
+            result[0].style.color = "red";
+        }
+    })
 }
-
-// The startGame function is called when the start button is clicked
-function startGame() {
-  isWin = false;
-  timerCount = 60;
-  // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  renderBlanks()
-  startTimer()
+  
+if (start) {
+    iterate("0");
 }
-
-// The winGame function is called when the win condition is met
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
-  startButton.disabled = false;
-  setWins()
-}
-
-// The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
-}
-
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-function startTimer() {
-  // Sets timer
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
+  
+// Next button and method
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+  
+next.addEventListener("click", () => {
+    start = false;
+    if (id < Questions.length) {
+        id++;
+        iterate(id);
+        console.log(id);
     }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
-  }, 1000);
-}
-
-// Creates blanks on screen
-function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
-  }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
-}
-
-// Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
-}
-
-// Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
-}
-
-// These functions are used by init
-function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
-  }
-  //Render win count to page
-  win.textContent = winCounter;
-}
-
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
-
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isWin = true;
-  }
-}
-
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
-
-// Attach event listener to document to listen for key event
-document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
 });
-
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
-
-// Calls init() so that it fires when page opened
-init();
-
-// Bonus: Add reset button
-var resetButton = document.querySelector(".reset-button");
-
-function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
-}
-// Attaches event listener to button
-resetButton.addEventListener("click", resetGame);
